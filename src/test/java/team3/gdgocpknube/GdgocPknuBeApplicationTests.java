11ba28224afcb8b8b48a619f679766fff23b1f47
박건현 <gun110297@naver.com>
2026-09-19 13:57:56 +0900

    feat: spring boot initallize

---
+++ src/test/java/team3/gdgocpknube/GdgocPknuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.gdgocpknube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgocPknuBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
